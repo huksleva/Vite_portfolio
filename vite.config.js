ac538ebb8b6f6c40e9144eac93abd96a2a27774c
Leonid <leonid005xc@gmail.com>
2026-02-26 20:42:49 +0300

    1234

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,28 @@
-import { resolve } from 'path'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default {
   root: resolve(__dirname, 'src'),
-  base: '/Vite_portfolio/',
+  base: '/Vite_portfolio/',  // Убедитесь, что имя репозитория точно такое же
   build: {
-    outDir: '../dist'
+    outDir: resolve(__dirname, '../dist'),
+    emptyOutDir: true,  // Очищает папку перед сборкой
   },
   server: {
     port: 8080
   },
-  // Optional: Silence Sass deprecation warnings. See note below.
   css: {
-     preprocessorOptions: {
-        scss: {
-          silenceDeprecations: [
-            'import',
-            'mixed-decls',
-            'color-functions',
-            'global-builtin',
-          ],
-        },
-     },
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: [
+          'import',
+          'mixed-decls',
+          'color-functions',
+          'global-builtin',
+        ],
+      },
+    },
   },
 }
